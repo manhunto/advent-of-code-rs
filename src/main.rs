00e6100de345db47f426ad9bd4bba924b2246a0b
Kuba Sładek <k.sladek@tidio.net>
2023-12-03 17:24:54 +0100

    01 Refactor

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,18 @@ fn main() {
     let input = read_to_string("src/01_puzzle.in")
         .expect("Should be able to read this file");
 
-    let lines = input.split("\n");
+    let lines: Vec<&str> = input.split("\n").collect();
 
+    let part_one: i32 = part_one(&lines);
+    let part_two: i32 = part_two(&lines);
+
+    println!("Part one: {}\nPart two: {}", part_one, part_two);
+}
+
+fn part_one(_lines: &Vec<&str>) -> i32 {
     let mut sum: i32 = 0;
 
-    for line in lines {
+    for line in _lines {
         let mut numbers = Vec::new();
 
         for char in line.chars() {
@@ -23,10 +30,9 @@ fn main() {
         sum += number_as_int;
     }
 
-    // let output = read_to_string("src/01_example.out")
-    //     .expect("Should be able to read this file");
-    //
-    // assert_eq!(output, sum.to_string());
+    sum
+}
 
-    println!("{}", sum);
+fn part_two(_lines: &Vec<&str>) -> i32 {
+    0
 }
