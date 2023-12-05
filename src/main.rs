899e573a0f635e4cbc478c73049690c645b4dfa5
Kuba Sładek <k.sladek@tidio.net>
2023-12-05 23:12:21 +0100

    01 Simplify code

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,30 @@ fn main() {
         .expect("Should be able to read this file");
 
     let start_part_one = Instant::now();
-    let part_one: u32 = part_one(&input.as_str());
+    let part_one: String = part_one(&input.as_str());
     let duration_part_one = start_part_one.elapsed();
 
     let start_part_two = Instant::now();
-    let part_two: u32 = part_two(&input.as_str());
+    let part_two: String = part_two(&input.as_str());
     let duration_part_two = start_part_two.elapsed();
 
     println!("Part one: {} ({:?})", part_one, duration_part_one);
     println!("Part two: {} ({:?})", part_two, duration_part_two);
 }
 
-fn part_one(input: &str) -> u32 {
+fn part_one(input: &str) -> String {
     input.lines()
-        .map(|l: &str| utils::calculate_line(l))
-        .sum()
+        .map(|line: &str| utils::calculate_line(line))
+        .sum::<u32>()
+        .to_string()
 }
 
-fn part_two(input: &str) -> u32 {
+fn part_two(input: &str) -> String {
     input.lines()
         .map(|l: &str| {
             let new = utils::replace_words_to_numbers(l);
             utils::calculate_line(new.as_str())
         })
-        .sum()
+        .sum::<u32>()
+        .to_string()
 }
