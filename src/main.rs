a607d24585069e2db9b5d8f3613a66324b599cda
Kuba Sładek <k.sladek@tidio.net>
2023-12-05 22:30:10 +0100

    01 Add tests

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,25 @@ fn main() {
     // let _type = "example";
     let _type = "puzzle";
 
-    let input1 = read_to_string(format!("src/01_{}.in", _type))
+    let input = read_to_string(format!("src/01_{}.in", _type))
         .expect("Should be able to read this file");
 
-    let lines1: Vec<&str> = input1.split("\n").collect();
+    let lines1: Vec<&str> = input.split("\n").collect();
 
     let start_part_one = Instant::now();
-    let part_one: i32 = part_one(&lines1);
+    let part_one: u32 = part_one(&lines1);
     let duration_part_one = start_part_one.elapsed();
 
     let start_part_two = Instant::now();
-    let part_two: i32 = part_two(&lines1);
+    let part_two: u32 = part_two(&lines1);
     let duration_part_two = start_part_two.elapsed();
 
     println!("Part one: {} ({:?})", part_one, duration_part_one);
     println!("Part two: {} ({:?})", part_two, duration_part_two);
 }
 
-fn part_one(_lines: &Vec<&str>) -> i32 {
-    let mut sum: i32 = 0;
+fn part_one(_lines: &Vec<&str>) -> u32 {
+    let mut sum: u32 = 0;
 
     for line in _lines {
         sum += utils::calculate_line(line);
@@ -34,13 +34,15 @@ fn part_one(_lines: &Vec<&str>) -> i32 {
     sum
 }
 
-fn part_two(_lines: &Vec<&str>) -> i32 {
-    let mut sum: i32 = 0;
+fn part_two(_lines: &Vec<&str>) -> u32 {
+    let mut sum: u32 = 0;
 
     for line in _lines {
         let new = utils::replace_words_to_numbers(&line);
 
-        sum += utils::calculate_line(new.as_str());
+        let val = utils::calculate_line(new.as_str());
+        println!("{} -> {} -> {}", line, new, val);
+        sum += val;
     }
 
     sum
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,5 @@
 pub fn replace_words_to_numbers(x: &str) -> String {
-    for i in 1..x.len() + 1 {
+    for i in 3..x.len() + 1 {
         let part: &str = &x[0..i];
         let replaced_part = part
             .replace("one", "1")
@@ -10,8 +10,7 @@ pub fn replace_words_to_numbers(x: &str) -> String {
             .replace("six", "6")
             .replace("seven", "7")
             .replace("eight", "8")
-            .replace("nine", "9")
-            ;
+            .replace("nine", "9");
 
         if part != replaced_part.as_str() {
             let rest = &x[i..x.len()];
@@ -23,7 +22,7 @@ pub fn replace_words_to_numbers(x: &str) -> String {
     return String::from(x);
 }
 
-pub fn calculate_line(line: &str) -> i32 {
+pub fn calculate_line(line: &str) -> u32 {
     let mut numbers = Vec::new();
 
     for char in line.chars() {
@@ -32,14 +31,15 @@ pub fn calculate_line(line: &str) -> i32 {
         }
     }
 
-    let number = format!("{}{}", numbers.first().unwrap(), numbers.last().unwrap());
+    let first = numbers.first().unwrap().to_digit(10).unwrap();
+    let last = numbers.last().unwrap().to_digit(10).unwrap();
 
-    number.parse().unwrap()
+    first * 10 + last
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::utils::replace_words_to_numbers;
+    use crate::utils::{calculate_line, replace_words_to_numbers};
 
     #[test]
     fn replace_words_to_numbers_test() {
@@ -53,5 +53,25 @@ mod tests {
         assert_eq!(replace_words_to_numbers("4nineeightseven2"), "49872");
         assert_eq!(replace_words_to_numbers("zoneight234"), "z1ight234");
         assert_eq!(replace_words_to_numbers("7pqrstsixteen"), "7pqrst6teen");
+        assert_eq!(replace_words_to_numbers("fivethreeonezblqnsfk1"), "531zblqnsfk1");
+        assert_eq!(replace_words_to_numbers("two74119onebtqgnine"), "2741191btqg9");
+        assert_eq!(replace_words_to_numbers("jrjh5vsrxbhsfour3"), "jrjh5vsrxbhs43");
+    }
+
+    #[test]
+    fn calculate_line_test() {
+        assert_eq!(calculate_line("12"), 12);
+        assert_eq!(calculate_line("219"), 29);
+        assert_eq!(calculate_line("8wo3"), 83);
+        assert_eq!(calculate_line("abc123xyz"), 13);
+        assert_eq!(calculate_line("x2ne34"), 24);
+        assert_eq!(calculate_line("49872"), 42);
+        assert_eq!(calculate_line("z1ight234"), 14);
+        assert_eq!(calculate_line("7pqrst6teen"), 76);
+        assert_eq!(calculate_line("531zblqnsfk1"), 51);
+        assert_eq!(calculate_line("2741191btqg9"), 29);
+        assert_eq!(calculate_line("jrjh5vsrxbhs43"), 53);
+        assert_eq!(calculate_line("jrjhvsrxbhs43"), 43);
+        assert_eq!(calculate_line("treb7uchet"), 77);
     }
 }
