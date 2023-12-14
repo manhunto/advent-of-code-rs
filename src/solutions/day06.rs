91558fb0b91cf7e885b7871a8a84731b8d7b63d1
Kuba Sładek <k.sladek@tidio.net>
2023-12-14 14:35:30 +0100

    06 Parse input

---
+++ src/solutions/day06.rs
@@ -0,0 +1,75 @@
+use crate::solutions::Solution;
+
+pub struct Day06;
+
+impl Solution for Day06 {
+    fn part_one(&self, input: &str) -> String {
+        String::from("0")
+    }
+
+    fn part_two(&self, input: &str) -> String {
+        String::from("0")
+    }
+}
+
+fn parse_input(input: &str) -> Vec<RaceInfo> {
+    let mut lines = input.lines();
+
+    let get_numbers_from_line = |line: Option<&str>| -> Vec<i32> {
+        line
+            .unwrap()
+            .split_whitespace()
+            .filter_map(|part| part.parse::<i32>().ok())
+            .collect()
+    };
+
+    let times: Vec<i32> = get_numbers_from_line(lines.next());
+    let distances: Vec<i32> = get_numbers_from_line(lines.next());
+
+    times
+        .iter()
+        .enumerate()
+        .map(|(i, t)| {
+            RaceInfo::new(
+                *t,
+                *distances.get(i).unwrap()
+            )
+        }).collect()
+}
+
+#[derive(PartialEq,Debug)]
+struct RaceInfo {
+    time: i32,
+    distance_to_beat: i32,
+}
+
+impl RaceInfo {
+    fn new(time: i32, distance_to_beat: i32) -> Self {
+        Self { time, distance_to_beat }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::file_system::read_example;
+    use crate::solutions::day06::{Day06, parse_input, RaceInfo};
+    use crate::solutions::Solution;
+
+    #[test]
+    fn part_one_example_test() {
+        let input = read_example("06");
+
+        assert_eq!("288", Day06.part_one(&input.as_str()));
+    }
+
+    #[test]
+    fn parse_input_test() {
+        let input = read_example("06");
+
+        assert_eq!(vec![
+            RaceInfo::new(7, 9),
+            RaceInfo::new(15, 40),
+            RaceInfo::new(30, 200),
+        ], parse_input(&input))
+    }
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -3,18 +3,20 @@ pub mod day02;
 pub mod day03;
 pub mod day04;
 pub mod day05;
+pub mod day06;
 
 pub trait Solution {
     fn part_one(&self, input: &str) -> String;
     fn part_two(&self, input: &str) -> String;
 }
 
-pub fn get_solutions() -> [Box<dyn Solution>; 5] {
+pub fn get_solutions() -> [Box<dyn Solution>; 6] {
     [
         Box::new(day01::Day01),
         Box::new(day02::Day02),
         Box::new(day03::Day03),
         Box::new(day04::Day04),
         Box::new(day05::Day05),
+        Box::new(day06::Day06),
     ]
 }
