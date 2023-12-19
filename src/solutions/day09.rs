06a29b49b4be36c0bcaf9b872bbfac0bd95fb0de
Kuba Sładek <k.sladek@tidio.net>
2023-12-19 02:47:01 +0100

    09 Solve first part

--- src/solutions/day09.rs
+++ src/solutions/day09.rs
@@ -4,7 +4,13 @@ pub struct Day09;
 
 impl Solution for Day09 {
     fn part_one(&self, input: &str) -> String {
-        String::from("0")
+        let history = self.parse_input(input);
+
+        history
+            .iter()
+            .map(|h| self.solve(h.clone()))
+            .sum::<i32>()
+            .to_string()
     }
 
     fn part_two(&self, input: &str) -> String {
@@ -24,6 +30,40 @@ impl Day09 {
             })
             .collect()
     }
+
+    fn solve(&self, history: Vec<i32>) -> i32 {
+        let mut last: Vec<i32> = history.clone();
+        let mut differences: Vec<Vec<i32>> = vec![last.clone()];
+
+        loop {
+            let mut t: Vec<i32> = vec![];
+            for i in 1..last.len() {
+                t.push(last[i] - last[i - 1]);
+            }
+
+            differences.push(t.clone());
+            last = t.clone();
+
+            let not_zeros: Vec<i32> = last.clone()
+                .into_iter()
+                .filter(|n| !n.eq(&0i32))
+                .collect();
+
+            if not_zeros.is_empty() {
+                break;
+            }
+        }
+
+        let mut tmp: i32 = 0;
+
+        for i in (0..differences.len() - 1).rev() {
+            let a = differences[i].last().unwrap();
+
+            tmp += a;
+        }
+
+        tmp
+    }
 }
 
 #[cfg(test)]
@@ -37,6 +77,13 @@ mod tests {
         let input = read_example("09");
 
         assert_eq!("114", Day09.part_one(&input.as_str()));
+    }
+
+    #[test]
+    fn solve() {
+        assert_eq!(18, Day09.solve(vec![0, 3, 6, 9, 12, 15]));
+        assert_eq!(28, Day09.solve(vec![1, 3, 6, 10, 15, 21]));
+        assert_eq!(68, Day09.solve(vec![10, 13, 16, 21, 30, 45]));
     }
 
     #[test]
