6084f8b2b96ed0bf8d74c3e5d4661144b79b643a
Kuba Sładek <k.sladek@tidio.net>
2023-12-22 01:03:03 +0100

    11 Expand universe

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -8,20 +8,9 @@ pub struct Day11;
 
 impl Solution for Day11 {
     fn part_one(&self, input: &str) -> String {
-        let grid: Grid<char> =  Grid::from(input);
+        let grid: Grid<char> = self.parse_input(input);
 
-        let rows = grid.rows();
-        let rows_without_galaxy: Vec<&i32> = self.get_empty(&rows);
-
-        let columns = grid.columns();
-        let columns_without_galaxy: Vec<&i32> = self.get_empty(&columns);
-
-
-
-        println!("{}", grid.to_string());
-
-        println!("{:?}", rows_without_galaxy);
-        println!("{:?}", columns_without_galaxy);
+        // println!("{}", grid.to_string());
 
         String::from('0')
     }
@@ -32,7 +21,27 @@ impl Solution for Day11 {
 }
 
 impl Day11 {
-    fn get_empty<'a>(&'a self, data: &'a HashMap<i32, HashMap<&Point, &char>>) -> Vec<&i32>{
+    fn parse_input(&self, input: &str) -> Grid<char> {
+        let mut grid: Grid<char> =  Grid::from(input);
+
+        let rows = grid.rows();
+        let rows_without_galaxy: Vec<i32> = self.get_empty(&rows);
+
+        let columns = grid.columns();
+        let columns_without_galaxy: Vec<i32> = self.get_empty(&columns);
+
+        for row in &rows_without_galaxy {
+            grid.insert_row(row.clone(), '.')
+        }
+
+        for column in &columns_without_galaxy {
+            grid.insert_column(column.clone(), '.')
+        }
+
+        grid
+    }
+
+    fn get_empty<'a>(&'a self, data: &'a HashMap<i32, HashMap<&Point, &char>>) -> Vec<i32>{
         data
             .iter()
             .filter(|(_, &ref element)| {
@@ -40,8 +49,9 @@ impl Day11 {
                     .iter()
                     .all(|(_, &c)| c == &'.')
             })
-            .map(|(i, _)| i)
+            .map(|(i, _)| i.clone())
             .sorted()
+            .rev()
             .collect()
     }
 }
@@ -51,6 +61,27 @@ mod tests {
     use crate::file_system::read_example;
     use crate::solutions::day11::Day11;
     use crate::solutions::Solution;
+
+    #[test]
+    fn parse_input() {
+        let input = read_example("11");
+
+        let expected = "....#........
+.........#...
+#............
+.............
+.............
+........#....
+.#...........
+............#
+.............
+.............
+.........#...
+#....#.......
+";
+
+        assert_eq!(expected, Day11.parse_input(&input.as_str()).to_string());
+    }
 
     #[test]
     fn part_one_example_test() {
