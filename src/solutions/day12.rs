eddd371e6ec307fabe4226805e1ac0a32620db9d
Kuba Sładek <k.sladek@tidio.net>
2023-12-24 11:03:33 +0100

    12 Implement validation spring method

--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -17,37 +17,50 @@ impl Solution for Day12 {
 }
 
 impl Day12 {
-    fn parse_input(input: &str) -> Vec<ConditionRecord>{
+    fn parse_input(input: &str) -> Vec<ConditionRecord> {
         input
             .lines()
-            .map(Self::parse_line)
+            .map(ConditionRecord::from)
             .collect()
-    }
-
-    fn parse_line(line: &str) -> ConditionRecord {
-        let mut groups = line.split_whitespace();
-
-        let pattern: Vec<Spring> = groups.next().unwrap().chars().map(Spring::from).collect();
-        let order: Vec<i32> = groups.next().unwrap().chars().filter_map(|c| c.to_string().parse().ok()).collect();
-
-        ConditionRecord {
-            pattern,
-            order
-        }
     }
 }
 
 #[derive(Debug)]
 struct ConditionRecord {
     pattern: Vec<Spring>,
-    order: Vec<i32>
+    order: Vec<i32>,
 }
 
-#[derive(Debug)]
+impl From<&str> for ConditionRecord {
+    fn from(value: &str) -> Self {
+        let mut groups = value.split_whitespace();
+
+        let pattern: Vec<Spring> = groups.next().unwrap().chars().map(Spring::from).collect();
+        let order: Vec<i32> = groups.next().unwrap().chars().filter_map(|c| c.to_string().parse().ok()).collect();
+
+        ConditionRecord {
+            pattern,
+            order,
+        }
+    }
+}
+
+impl ConditionRecord {
+    fn is_valid(&self) -> bool {
+        let pattern_to_order: Vec<i32> = self.pattern
+            .split(|s| s != &Spring::Damaged)
+            .filter_map(|g| if g.is_empty() { None } else { Some(g.len() as i32) })
+            .collect();
+
+        pattern_to_order == self.order
+    }
+}
+
+#[derive(PartialEq, Debug)]
 enum Spring {
     Operational,
     Damaged,
-    Unknown
+    Unknown,
 }
 
 impl From<char> for Spring {
@@ -64,7 +77,7 @@ impl From<char> for Spring {
 #[cfg(test)]
 mod tests {
     use crate::file_system::read_example;
-    use crate::solutions::day12::Day12;
+    use crate::solutions::day12::{ConditionRecord, Day12};
     use crate::solutions::Solution;
 
     #[test]
@@ -72,5 +85,21 @@ mod tests {
         let input = read_example("12");
 
         assert_eq!("21", Day12.part_one(&input.as_str()));
+    }
+
+    #[test]
+    fn condition_record_is_valid_test() {
+        assert!(ConditionRecord::from("#.#.### 1,1,3").is_valid());
+        assert!(ConditionRecord::from(".#...#....###. 1,1,3").is_valid());
+        assert!(ConditionRecord::from(".#.###.#.###### 1,3,1,6").is_valid());
+        assert!(ConditionRecord::from("####.#...#... 4,1,1").is_valid());
+        assert!(ConditionRecord::from("#....######..#####. 1,6,5").is_valid());
+        assert!(ConditionRecord::from(".###.##....# 3,2,1").is_valid());
+        assert!(ConditionRecord::from("## 2").is_valid());
+        assert!(ConditionRecord::from("#. 1").is_valid());
+        assert!(ConditionRecord::from(".# 1").is_valid());
+
+        assert!(!ConditionRecord::from(".# 2").is_valid());
+        assert!(!ConditionRecord::from("?.?.### 1,1,3").is_valid());
     }
 }
