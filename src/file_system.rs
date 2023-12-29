35e336d7b9a9e90ee04d8c873acd207af2a0ae52
Kuba Sładek <k.sladek@tidio.net>
2023-12-29 15:00:51 +0100

    Allow to compare with expected result

--- src/file_system.rs
+++ src/file_system.rs
@@ -1,15 +1,18 @@
 use std::fs::read_to_string;
 
 pub fn read_input(day: &str) -> String {
-    read(format!("resources/inputs/{}.in", day))
+    read(format!("resources/inputs/{}.in", day)).unwrap()
+}
+
+pub fn read_output(day: &str) -> std::io::Result<String> {
+    read(format!("resources/outputs/{}.out", day))
 }
 
 #[cfg(test)]
 pub fn read_example(day: &str) -> String {
-    read(format!("resources/examples/{}.in", day))
+    read(format!("resources/examples/{}.in", day)).unwrap()
 }
 
-fn read(file_path: String) -> String {
+fn read(file_path: String) -> std::io::Result<String> {
     read_to_string(file_path)
-        .expect("Should be able to read this file")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
-use std::time::Instant;
+use std::fmt::{Display, Formatter};
+use std::time::{Duration, Instant};
 use crate::day::DayNumber;
 use crate::solutions::{get_solutions};
 
@@ -25,15 +26,52 @@ fn main() {
     let solution = &solutions[(day_number.as_u8() - 1) as usize];
 
     let input = file_system::read_input(day_number.as_string().as_str());
+    let output = file_system::read_output(day_number.as_string().as_str());
+
+    let lines: Vec<String> = output.unwrap_or(String::from("")).lines().map(|s|s.to_string()).collect();
+    let expected_part_one = lines.get(0);
+    let expected_part_two = lines.get(1);
 
     let start_part_one = Instant::now();
     let part_one: String = solution.part_one(&input.as_str());
     let duration_part_one = start_part_one.elapsed();
 
+    let part_one_result = Result {
+        expected: expected_part_one,
+        current: part_one.clone(),
+        elapsed: duration_part_one
+    };
+
     let start_part_two = Instant::now();
     let part_two: String = solution.part_two(&input.as_str());
     let duration_part_two = start_part_two.elapsed();
 
-    println!("Part one: {} ({:?}) ❔ ✅ ❌", part_one, duration_part_one);
-    println!("Part two: {} ({:?}) ❔ ✅ ❌", part_two, duration_part_two);
+    let part_two_result = Result {
+        expected: expected_part_two,
+        current: part_two.clone(),
+        elapsed: duration_part_two
+    };
+
+    println!("{}", part_one_result);
+    println!("{}", part_two_result);
+}
+struct Result<'a> {
+    expected: Option<&'a String>,
+    current: String,
+    elapsed: Duration,
+}
+
+impl Display for Result<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut result: &str;
+        if self.expected.is_none() {
+            result = "❔";
+        } else if self.expected.unwrap() == &self.current {
+            result = "✅";
+        } else {
+            result = "❌";
+        }
+
+        write!(f, "Part one: {} ({:?}) {}", self.current, self.elapsed, result)
+    }
 }
