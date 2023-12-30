be2706f2848635bff97a02bd20f9ead684904dc3
Kuba Sładek <k.sladek@tidio.net>
2023-12-30 23:01:35 +0100

    16 Simplify

--- src/solutions/day16.rs
+++ src/solutions/day16.rs
@@ -31,7 +31,7 @@ impl Solution for Day16 {
 }
 
 impl Day16 {
-    fn energize(start: Vector, grid: &Grid<char>) -> usize{
+    fn energize(start: Vector, grid: &Grid<char>) -> usize {
         let surface_range = grid.surface_range();
 
         let mut beams: Vec<Vector> = vec![start];
@@ -42,47 +42,39 @@ impl Day16 {
                 .into_iter()
                 .filter_map(|beam| {
                     let position = beam.position();
-                    if !surface_range.contains(position) {
+                    if !surface_range.contains(position) || history.contains(&beam){
                         return None;
                     }
 
-                    if history.contains(&beam) {
-                        return None;
-                    }
-
-                    let direction = beam.direction();
                     history.push(beam.clone());
 
                     let tile = grid.get_for_point(&position).unwrap();
-                    if *tile == '.' {
-                        return Some(vec![beam.do_move()]);
-                    } else if *tile == '|' {
-                        return Some(match direction {
+                    let direction = beam.direction();
+
+                    Some(match *tile {
+                        '.' => vec![beam.do_move()],
+                        '|' => match direction {
                             South | North => vec![beam.do_move()],
                             _ => vec![beam.rotate(South).do_move(), beam.rotate(North).do_move()]
-                        });
-                    } else if *tile == '-' {
-                        return Some(match direction {
+                        },
+                        '-' => match direction {
                             East | West => vec![beam.do_move()],
                             _ => vec![beam.rotate(East).do_move(), beam.rotate(West).do_move()]
-                        });
-                    } else if *tile == '/' {
-                        return Some(vec![match direction {
+                        },
+                        '/' => vec![match direction {
                             North => beam.rotate(East).do_move(),
                             East => beam.rotate(North).do_move(),
                             West => beam.rotate(South).do_move(),
                             South => beam.rotate(West).do_move(),
-                        }]);
-                    } else if *tile == '\\' {
-                        return Some(vec![match direction {
+                        }],
+                        '\\' => vec![match direction {
                             North => beam.rotate(West).do_move(),
                             East => beam.rotate(South).do_move(),
                             West => beam.rotate(North).do_move(),
                             South => beam.rotate(East).do_move(),
-                        }]);
-                    }
-
-                    panic!("{}", format!("Unrecognized {} {} {:?}", tile, position, direction));
+                        }],
+                        _ => panic!("{}", format!("Unrecognized {} {} {:?}", tile, position, direction))
+                    })
                 })
                 .flatten()
                 .collect();
@@ -90,7 +82,7 @@ impl Day16 {
 
         history
             .clone()
-            .into_iter()
+            .iter()
             .map(|b| b.position())
             .unique()
             .collect::<Vec<Point>>()
