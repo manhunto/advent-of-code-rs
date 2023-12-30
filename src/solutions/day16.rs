60efeb4aa62eb6942c808c2264b97d381b3651ad
Kuba Sładek <k.sladek@tidio.net>
2023-12-30 23:27:43 +0100

    16 Rename functions

--- src/solutions/day16.rs
+++ src/solutions/day16.rs
@@ -34,6 +34,9 @@ impl Day16 {
     fn energize(start: Vector, grid: &Grid<char>) -> usize {
         let surface_range = grid.surface_range();
 
+        // (tile, direction) -> Vec<Point> - historia wszystkich punktów przez jakie przechodził
+        //
+
         let mut beams: Vec<Vector> = vec![start];
         let mut history: Vec<Vector> = Vec::new();
 
@@ -49,29 +52,29 @@ impl Day16 {
                     history.push(beam.clone());
 
                     let tile = grid.get_for_point(&position).unwrap();
-                    let direction = beam.direction();
+                    let direction = beam.facing();
 
                     Some(match *tile {
-                        '.' => vec![beam.do_move()],
+                        '.' => vec![beam.step()],
                         '|' => match direction {
-                            South | North => vec![beam.do_move()],
-                            _ => vec![beam.rotate(South).do_move(), beam.rotate(North).do_move()]
+                            South | North => vec![beam.step()],
+                            _ => vec![beam.rotate(South).step(), beam.rotate(North).step()]
                         },
                         '-' => match direction {
-                            East | West => vec![beam.do_move()],
-                            _ => vec![beam.rotate(East).do_move(), beam.rotate(West).do_move()]
+                            East | West => vec![beam.step()],
+                            _ => vec![beam.rotate(East).step(), beam.rotate(West).step()]
                         },
                         '/' => vec![match direction {
-                            North => beam.rotate(East).do_move(),
-                            East => beam.rotate(North).do_move(),
-                            West => beam.rotate(South).do_move(),
-                            South => beam.rotate(West).do_move(),
+                            North => beam.rotate(East).step(),
+                            East => beam.rotate(North).step(),
+                            West => beam.rotate(South).step(),
+                            South => beam.rotate(West).step(),
                         }],
                         '\\' => vec![match direction {
-                            North => beam.rotate(West).do_move(),
-                            East => beam.rotate(South).do_move(),
-                            West => beam.rotate(North).do_move(),
-                            South => beam.rotate(East).do_move(),
+                            North => beam.rotate(West).step(),
+                            East => beam.rotate(South).step(),
+                            West => beam.rotate(North).step(),
+                            South => beam.rotate(East).step(),
                         }],
                         _ => panic!("{}", format!("Unrecognized {} {} {:?}", tile, position, direction))
                     })
--- src/utils/vector.rs
+++ src/utils/vector.rs
@@ -4,27 +4,27 @@ use crate::point::Point;
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub struct Vector {
     position: Point,
-    direction: Direction,
+    facing: Direction,
 }
 
 impl Vector {
-    pub fn new(position: Point, direction: Direction) -> Self {
-        Self { position, direction }
+    pub fn new(position: Point, facing: Direction) -> Self {
+        Self { position, facing }
     }
 
     pub fn position(&self) -> Point {
         self.position
     }
 
-    pub fn direction(&self) -> Direction {
-        self.direction
+    pub fn facing(&self) -> Direction {
+        self.facing
     }
 
-    pub fn do_move(&self) -> Self {
-        Self::new(self.position.move_in(self.direction), self.direction)
+    pub fn step(&self) -> Self {
+        Self::new(self.position.move_in(self.facing), self.facing)
     }
 
-    pub fn rotate(&self, direction: Direction) -> Self {
-        Self::new(self.position, direction)
+    pub fn rotate(&self, facing: Direction) -> Self {
+        Self::new(self.position, facing)
     }
 }
