a9bf8bf677d1e1b54cb21f5fd3cd7979b0d7378e
Kuba Sładek <jakub.sladek@zoho.com>
2024-12-05 20:17:43 +0100

    fix(03/2024): don't split input by new lines and solve the puzzle

--- src/solutions/year2024/day03.rs
+++ src/solutions/year2024/day03.rs
@@ -28,45 +28,40 @@ impl Solution for Day03 {
         let do_re = Regex::new(r"do\(\)").unwrap();
         let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
 
-        input
-            .lines()
-            .map(|line| {
-                let dont_positions: Vec<usize> = dont_re.find_iter(line).map(|m| m.end()).collect();
-                let do_positions: Vec<usize> = do_re.find_iter(line).map(|m| m.end()).collect();
+        let dont_positions: Vec<usize> = dont_re.find_iter(input).map(|m| m.end()).collect();
+        let do_positions: Vec<usize> = do_re.find_iter(input).map(|m| m.end()).collect();
 
-                re.captures_iter(line)
-                    .map(|capture| {
-                        let mul_position = capture.get(0).unwrap().end();
-                        let left = capture[1].parse::<usize>().unwrap();
-                        let right = capture[2].parse::<usize>().unwrap();
+        re.captures_iter(input)
+            .map(|capture| {
+                let mul_position = capture.get(0).unwrap().end();
+                let left = capture[1].parse::<usize>().unwrap();
+                let right = capture[2].parse::<usize>().unwrap();
 
-                        let do_matched = do_positions
-                            .clone()
-                            .into_iter()
-                            .filter(|x| x < &mul_position)
-                            .collect::<Vec<usize>>();
+                let do_matched = do_positions
+                    .clone()
+                    .into_iter()
+                    .filter(|x| x < &mul_position)
+                    .collect::<Vec<usize>>();
 
-                        let do_recent = do_matched.last().unwrap_or(&0);
+                let do_recent = do_matched.last().unwrap_or(&0);
 
-                        let dont_matched = dont_positions
-                            .clone()
-                            .into_iter()
-                            .filter(|x| x < &mul_position)
-                            .collect::<Vec<usize>>();
+                let dont_matched = dont_positions
+                    .clone()
+                    .into_iter()
+                    .filter(|x| x < &mul_position)
+                    .collect::<Vec<usize>>();
 
-                        let dont_recent_opt = dont_matched.last();
+                let dont_recent_opt = dont_matched.last();
 
-                        if dont_recent_opt.is_none() {
-                            return left * right;
-                        }
+                if dont_recent_opt.is_none() {
+                    return left * right;
+                }
 
-                        if dont_recent_opt.is_some_and(|dont_recent| dont_recent < do_recent) {
-                            return left * right;
-                        }
+                if dont_recent_opt.is_some_and(|dont_recent| dont_recent < do_recent) {
+                    return left * right;
+                }
 
-                        0usize
-                    })
-                    .sum::<usize>()
+                0usize
             })
             .sum::<usize>()
             .to_string()
@@ -85,6 +80,7 @@ mod tests {
 
     const MULTILINE_EXAMPLE: &str = r#"don't()mul(4,2)
     mul(3,5)
+    do()mul(2,2)
     "#;
 
     #[test]
@@ -97,6 +93,14 @@ mod tests {
         assert_eq!("48", Day03.part_two(EXAMPLE_TWO));
         assert_eq!("191274", Day03.part_two("?)do())mul(449,426)"));
         assert_eq!("0", Day03.part_two("?)do())don't()mul(2,3)"));
-        assert_eq!("15", Day03.part_two(MULTILINE_EXAMPLE));
+        assert_eq!("0", Day03.part_two("?)do())don't()3{}mul(2,3)"));
+        assert_eq!("20", Day03.part_two("?)do()don't()do()mul(5,4)"));
+        assert_eq!("24", Day03.part_two("?)do()mul(2,2)don't()do()mul(5,4)"));
+        assert_eq!(
+            "24",
+            Day03.part_two("?)do()mul(2,2)don't()mul(3,2)do()mul(5,4)")
+        );
+        assert_eq!("20", Day03.part_two("?)mul(5,4)"));
+        assert_eq!("4", Day03.part_two(MULTILINE_EXAMPLE));
     }
 }
