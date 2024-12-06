c6377574a1f2afe4b661a2eb0cbb02ceceddd608
Kuba Sładek <jakub.sladek@zoho.com>
2024-12-06 19:53:47 +0100

    feat(06/2024): solve part one

--- src/solutions/year2024/day06.rs
+++ src/solutions/year2024/day06.rs
@@ -1,10 +1,34 @@
 use crate::solutions::Solution;
+use crate::utils::direction::Direction::North;
+use crate::utils::grid::Grid;
+use crate::utils::point::Point;
+use crate::utils::vector::Vector;
+use std::collections::HashSet;
 
 pub struct Day06;
 
 impl Solution for Day06 {
-    fn part_one(&self, _input: &str) -> String {
-        String::from('0')
+    fn part_one(&self, input: &str) -> String {
+        let grid: Grid<char> = Grid::from(input);
+        let obstructions = grid.get_all_positions(&'#');
+        let guard = grid.get_first_position(&'^').unwrap();
+        let surface = grid.surface_range();
+
+        let mut guard = Vector::new(guard, North);
+        let mut visited_positions: HashSet<Point> = HashSet::new();
+
+        while surface.contains(guard.position()) {
+            visited_positions.insert(guard.position());
+
+            let next_position = guard.step();
+            if obstructions.contains(&next_position.position()) {
+                guard = guard.rotate_cw().step()
+            } else {
+                guard = next_position;
+            }
+        }
+
+        visited_positions.len().to_string()
     }
 
     fn part_two(&self, _input: &str) -> String {
@@ -29,7 +53,6 @@ mod tests {
 ......#..."#;
 
     #[test]
-    #[ignore]
     fn part_one_example_test() {
         assert_eq!("41", Day06.part_one(EXAMPLE));
     }
