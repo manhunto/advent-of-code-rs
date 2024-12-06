b4f87ba87341bb70d0a0d3edf3c3e2e7dfc9c455
Kuba Sładek <jakub.sladek@zoho.com>
2024-12-07 00:12:35 +0100

    feat(06/2024): solve part two for example; for input it is slow and gives wrong answer

--- src/solutions/year2024/day06.rs
+++ src/solutions/year2024/day06.rs
@@ -3,22 +3,64 @@ use crate::utils::direction::Direction::North;
 use crate::utils::grid::Grid;
 use crate::utils::point::Point;
 use crate::utils::vector::Vector;
-use std::collections::HashSet;
+use itertools::Itertools;
 
 pub struct Day06;
 
 impl Solution for Day06 {
     fn part_one(&self, input: &str) -> String {
         let grid: Grid<char> = Grid::from(input);
+
+        self.generate_history(&grid).0.len().to_string()
+    }
+
+    fn part_two(&self, input: &str) -> String {
+        let grid: Grid<char> = Grid::from(input);
+        let guard = grid.get_first_position(&'^').unwrap();
+
+        self.generate_history(&grid)
+            .0
+            .into_iter()
+            .skip(1)
+            .filter(|v| {
+                if v == &guard {
+                    return false;
+                }
+
+                let mut grid_with_obstacle = grid.clone();
+                grid_with_obstacle.modify(*v, '#');
+
+                self.generate_history(&grid_with_obstacle).1 == Reason::Loop
+            })
+            .count()
+            .to_string()
+    }
+}
+
+impl Day06 {
+    fn generate_history(&self, grid: &Grid<char>) -> (Vec<Point>, Reason) {
         let obstructions = grid.get_all_positions(&'#');
         let guard = grid.get_first_position(&'^').unwrap();
         let surface = grid.surface_range();
 
         let mut guard = Vector::new(guard, North);
-        let mut visited_positions: HashSet<Point> = HashSet::new();
+        let mut visited_positions: Vec<Vector> = Vec::new();
 
-        while surface.contains(guard.position()) {
-            visited_positions.insert(guard.position());
+        let reason: Reason;
+
+        loop {
+            if !surface.contains(guard.position()) {
+                reason = Reason::OutOfSurface;
+                break;
+            }
+
+            // todo remove this visited_positions contains for part one, because it slows it down
+            if visited_positions.contains(&guard) {
+                reason = Reason::Loop;
+                break;
+            }
+
+            visited_positions.push(guard);
 
             let next_position = guard.step();
             if obstructions.contains(&next_position.position()) {
@@ -28,12 +70,21 @@ impl Solution for Day06 {
             }
         }
 
-        visited_positions.len().to_string()
+        (
+            visited_positions
+                .into_iter()
+                .map(|v| v.position())
+                .unique()
+                .collect(),
+            reason,
+        )
     }
+}
 
-    fn part_two(&self, _input: &str) -> String {
-        String::from('0')
-    }
+#[derive(PartialEq)]
+enum Reason {
+    OutOfSurface,
+    Loop,
 }
 
 #[cfg(test)]
@@ -55,5 +106,10 @@ mod tests {
     #[test]
     fn part_one_example_test() {
         assert_eq!("41", Day06.part_one(EXAMPLE));
+    }
+
+    #[test]
+    fn part_two_example_test() {
+        assert_eq!("6", Day06.part_two(EXAMPLE));
     }
 }
