9133a21deb641f3df28622c7b3a069e47da24c3e
Kuba Sładek <jakub.sladek@zoho.com>
2024-12-09 16:54:34 +0100

    feat(09/2024): implement parse

--- src/solutions/year2024/day09.rs
+++ src/solutions/year2024/day09.rs
@@ -1,4 +1,7 @@
 use crate::solutions::Solution;
+use itertools::Itertools;
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
 
 pub struct Day09;
 
@@ -12,15 +15,78 @@ impl Solution for Day09 {
     }
 }
 
+struct DiskMap {
+    blocks: Vec<Option<usize>>,
+}
+
+impl FromStr for DiskMap {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut current_id = 0;
+
+        let test: Vec<Option<usize>> = s
+            .chars()
+            .enumerate()
+            .flat_map(|(i, c)| {
+                let times: usize = c.to_string().parse().unwrap();
+
+                let value: Option<usize> = match i % 2 == 0 {
+                    true => {
+                        let id = Some(current_id);
+
+                        current_id += 1;
+
+                        id
+                    }
+                    false => None,
+                };
+
+                vec![value; times]
+            })
+            .collect();
+
+        Ok(Self { blocks: test })
+    }
+}
+
+impl Display for DiskMap {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let value = self
+            .blocks
+            .iter()
+            .map(|v| match v {
+                None => '.',
+                Some(v) => (v % 10).to_string().chars().next().unwrap(),
+            })
+            .join("");
+
+        write!(f, "{}", value)
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::solutions::year2024::day09::Day09;
+    use crate::solutions::year2024::day09::{Day09, DiskMap};
     use crate::solutions::Solution;
+    use std::str::FromStr;
 
     const EXAMPLE: &str = r#""#;
 
     #[test]
     fn part_one_example_test() {
         assert_eq!("0", Day09.part_one(EXAMPLE));
+    }
+
+    #[test]
+    fn parse_test() {
+        let result = DiskMap::from_str("12345").unwrap();
+        assert_eq!("0..111....22222", result.to_string());
+
+        let result = DiskMap::from_str("2333133121414131402").unwrap();
+        assert_eq!(
+            "00...111...2...333.44.5555.6666.777.888899",
+            result.to_string()
+        );
     }
 }
