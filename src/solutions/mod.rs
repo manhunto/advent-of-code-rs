68dc99836cd10ddaacc6d331277704d742a85783
Kuba Sładek <jakub.sladek@zoho.com>
2024-12-17 17:14:01 +0100

    chore(16/2024): add day boilerplate

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -29,6 +29,7 @@ pub fn solution(day: DayNumber, year: Year) -> Box<dyn Solution> {
             13 => Box::new(year2024::day13::Day13),
             14 => Box::new(year2024::day14::Day14::default()),
             15 => Box::new(year2024::day15::Day15),
+            16 => Box::new(year2024::day16::Day16),
             _ => panic!("Day not exist"),
         },
         Year::Year2023 => match i {
