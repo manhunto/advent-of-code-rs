c5e8642bba6911babd6b23c4717d66b9378d3695
Kuba Sładek <jakub.sladek@zoho.com>
2025-12-06 02:30:31 +0100

    feat(04/2025): solve first part

--- src/solutions/year2025/day04.rs
+++ src/solutions/year2025/day04.rs
@@ -1,10 +1,24 @@
 use crate::solutions::Solution;
+use crate::utils::grid::Grid;
 
 pub struct Day04;
 
 impl Solution for Day04 {
-    fn part_one(&self, _input: &str) -> String {
-        String::from("0")
+    fn part_one(&self, input: &str) -> String {
+        const ROLL_OF_PAPER: char = '@';
+        let grid: Grid<char> = Grid::from(input);
+
+        grid.get_all_positions(&ROLL_OF_PAPER)
+            .iter()
+            .filter(|p| {
+                p.adjacent_with_diagonal_vectors()
+                    .iter()
+                    .filter(|adj| grid.is_for_point(&adj.position(), ROLL_OF_PAPER))
+                    .count()
+                    < 4
+            })
+            .count()
+            .to_string()
     }
 
     fn part_two(&self, _input: &str) -> String {
@@ -17,10 +31,19 @@ mod tests {
     use crate::solutions::year2025::day04::Day04;
     use crate::solutions::Solution;
 
-    const EXAMPLE: &str = r#""#;
+    const EXAMPLE: &str = r#"..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."#;
 
     #[test]
     fn part_one_example_test() {
-        assert_eq!("0", Day04.part_one(EXAMPLE));
+        assert_eq!("13", Day04.part_one(EXAMPLE));
     }
 }
