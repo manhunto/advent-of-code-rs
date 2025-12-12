f9010caac54b6ca482cdbef5aea10b13b0af6d49
Kuba Sładek <jakub.sladek@zoho.com>
2025-12-12 16:18:00 +0100

    feat(04/2015): solve first part for example, but is slow

--- src/solutions/year2015/day04.rs
+++ src/solutions/year2015/day04.rs
@@ -1,10 +1,23 @@
 use crate::solutions::Solution;
+use md5::compute;
 
 pub struct Day04;
 
 impl Solution for Day04 {
-    fn part_one(&self, _input: &str) -> String {
-        String::from("0")
+    fn part_one(&self, input: &str) -> String {
+        for answer in 0u64.. {
+            // println!("{}", answer);
+            let hash = format!("{}{}", input, answer);
+            let digest = compute(hash);
+
+            let x = format!("{:x}", digest);
+            // println!("{}", x);
+            if &x[0..5] == "00000" {
+                return answer.to_string();
+            }
+        }
+
+        unreachable!();
     }
 
     fn part_two(&self, _input: &str) -> String {
@@ -18,6 +31,7 @@ mod tests {
 
     #[test]
     fn part_one_example_test() {
-        assert_eq!("0", Day04.part_one(""));
+        assert_eq!("609043", Day04.part_one("abcdef"));
+        assert_eq!("1048970", Day04.part_one("pqrstuv"));
     }
 }
