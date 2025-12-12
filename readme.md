15fd1bc58f9796c1992bfc16fd4199661580e1a7
Kuba Sładek <jakub.sladek@zoho.com>
2025-12-12 14:52:25 +0100

    feat(03/2015): solve first part

--- readme.md
+++ readme.md
@@ -85,10 +85,11 @@
 
 # 2015
 
-| Day                                                                         | Solved | Part 1 time (ms) | Part 2 time (ms) |
-|-----------------------------------------------------------------------------|:------:|-----------------:|-----------------:|
-| [Day 1: Not Quite Lisp](src/solutions/year2015/day01.rs)                    |   ⭐⭐   |            0.013 |            0.001 |
-| [Day 2: I Was Told There Would Be No Math](src/solutions/year2015/day02.rs) |   ⭐⭐   |            0.074 |            0.105 |
+| Day                                                                              | Solved | Part 1 time (ms) | Part 2 time (ms) |
+|----------------------------------------------------------------------------------|:------:|-----------------:|-----------------:|
+| [Day 1: Not Quite Lisp](src/solutions/year2015/day01.rs)                         |   ⭐⭐   |            0.013 |            0.001 |
+| [Day 2: I Was Told There Would Be No Math](src/solutions/year2015/day02.rs)      |   ⭐⭐   |            0.074 |            0.105 |
+| [Day 3: Perfectly Spherical Houses in a Vacuum](src/solutions/year2015/day03.rs) |   ⭐    |            0.358 |                - |
 
 # TODO
 
--- src/solutions/year2015/day03.rs
+++ src/solutions/year2015/day03.rs
@@ -1,10 +1,31 @@
 use crate::solutions::Solution;
+use crate::utils::point::Point;
+use std::collections::HashMap;
 
 pub struct Day03;
 
 impl Solution for Day03 {
-    fn part_one(&self, _input: &str) -> String {
-        String::from("0")
+    fn part_one(&self, input: &str) -> String {
+        let mut visited_houses: HashMap<Point, u64> = HashMap::new();
+        let mut current = Point::new(0, 0);
+
+        visited_houses.insert(current, 1);
+
+        for b in input.bytes() {
+            current = match b {
+                b'>' => current.east(),
+                b'<' => current.west(),
+                b'^' => current.north(),
+                b'v' => current.south(),
+                _ => unreachable!(),
+            };
+            visited_houses
+                .entry(current)
+                .and_modify(|e| *e += 1)
+                .or_insert(1);
+        }
+
+        visited_houses.keys().count().to_string()
     }
 
     fn part_two(&self, _input: &str) -> String {
@@ -18,6 +39,8 @@ mod tests {
 
     #[test]
     fn part_one_example_test() {
-        assert_eq!("0", Day03.part_one("0"));
+        assert_eq!("2", Day03.part_one(">"));
+        assert_eq!("4", Day03.part_one("^>v<"));
+        assert_eq!("2", Day03.part_one("^v^v^v^v^v"));
     }
 }
