bebf7a5980067e614f446832045ab221b40b294a
Kuba Sładek <jakub.sladek@zoho.com>
2025-12-12 15:15:21 +0100

    refactor(03/2015): extract common logic to one method

--- src/solutions/year2015/day03.rs
+++ src/solutions/year2015/day03.rs
@@ -8,46 +8,34 @@ pub struct Day03;
 
 impl Solution for Day03 {
     fn part_one(&self, input: &str) -> String {
-        let mut visited_houses: Houses = HashMap::new();
-        let mut santa = Point::new(0, 0);
-        visited_houses.insert(santa, 1);
-
-        for b in input.bytes() {
-            santa = self.make_move(santa, b);
-
-            self.visit_house(&mut visited_houses, santa);
-        }
-
-        visited_houses.len().to_string()
+        self.solve(input, 1).to_string()
     }
 
     fn part_two(&self, input: &str) -> String {
-        let mut visited_houses: Houses = HashMap::new();
-        let mut santa = Point::new(0, 0);
-        visited_houses.insert(santa, 1);
-
-        let mut robo_santa = Point::new(0, 0);
-        visited_houses.insert(robo_santa, 1);
-
-        for (i, b) in input.bytes().enumerate() {
-            let visited = if i % 2 == 0 {
-                santa = self.make_move(santa, b);
-
-                santa
-            } else {
-                robo_santa = self.make_move(robo_santa, b);
-
-                robo_santa
-            };
-
-            self.visit_house(&mut visited_houses, visited);
-        }
-
-        visited_houses.len().to_string()
+        self.solve(input, 2).to_string()
     }
 }
 
 impl Day03 {
+    fn solve(&self, input: &str, santas: usize) -> usize {
+        let mut visited_houses: Houses = HashMap::new();
+        let mut santas = vec![Point::new(0, 0); santas];
+
+        for santa in &santas {
+            visited_houses.insert(*santa, 1);
+        }
+
+        for (i, b) in input.bytes().enumerate() {
+            let index = i % santas.len();
+
+            santas[index] = self.make_move(santas[index], b);
+
+            self.visit_house(&mut visited_houses, santas[index]);
+        }
+
+        visited_houses.len()
+    }
+
     fn make_move(&self, point: Point, b: u8) -> Point {
         match b {
             b'>' => point.east(),
@@ -59,7 +47,7 @@ impl Day03 {
     }
 
     fn visit_house(&self, houses: &mut Houses, house: Point) {
-        houses.entry(house).and_modify(|e| *e += 1).or_insert(1);
+        *houses.entry(house).or_default() += 1
     }
 }
 
