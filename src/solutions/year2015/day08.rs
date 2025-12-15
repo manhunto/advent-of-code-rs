ba0798f5e3434bb3dd808e218b312bac7667a905
Kuba Sładek <jakub.sladek@zoho.com>
2025-12-15 23:41:22 +0100

    feat(08/2015): solve first part

--- src/solutions/year2015/day08.rs
+++ src/solutions/year2015/day08.rs
@@ -3,8 +3,36 @@ use crate::solutions::Solution;
 pub struct Day08;
 
 impl Solution for Day08 {
-    fn part_one(&self, _input: &str) -> String {
-        String::from("0")
+    fn part_one(&self, input: &str) -> String {
+        input
+            .lines()
+            .map(|l| {
+                let mut i: usize = 1;
+                let len = l.len();
+                let mut characters: usize = 0;
+
+                loop {
+                    if i == len - 1 {
+                        break;
+                    }
+
+                    if l.chars().nth(i).unwrap() == '\\' {
+                        if l.chars().nth(i + 1).unwrap() == 'x' {
+                            i += 3;
+                        } else {
+                            i += 1;
+                        }
+                    }
+
+                    characters += 1;
+
+                    i += 1;
+                }
+
+                len - characters
+            })
+            .sum::<usize>()
+            .to_string()
     }
 
     fn part_two(&self, _input: &str) -> String {
@@ -18,6 +46,9 @@ mod tests {
 
     #[test]
     fn part_one_example_test() {
-        assert_eq!("0", Day08.part_one("0"));
+        assert_eq!("2", Day08.part_one("\"\""));
+        assert_eq!("2", Day08.part_one("\"abc\""));
+        assert_eq!("3", Day08.part_one("\"aaa\\\"aaa\""));
+        assert_eq!("5", Day08.part_one("\"\\x27\""));
     }
 }
