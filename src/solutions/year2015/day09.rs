67761cf3b1a5379e4e8477ca160f618e92cd0386
Kuba Sładek <jakub.sladek@zoho.com>
2025-12-16 00:50:32 +0100

    feat(09/2015): implement parse

--- src/solutions/year2015/day09.rs
+++ src/solutions/year2015/day09.rs
@@ -1,10 +1,26 @@
 use crate::solutions::Solution;
+use itertools::Itertools;
+use std::collections::HashMap;
+
+type Distances = HashMap<(String, String), u64>;
 
 pub struct Day09;
 
 impl Solution for Day09 {
-    fn part_one(&self, _input: &str) -> String {
-        String::from("0")
+    fn part_one(&self, input: &str) -> String {
+        let distances = self.parse(input);
+        let cities = self.cities(&distances);
+
+        println!("cities: {:?}", cities);
+        println!("distances: {:?}", distances);
+
+        cities
+            .iter()
+            .tuple_combinations()
+            .map(|(_, _)| 0)
+            .min()
+            .unwrap()
+            .to_string()
     }
 
     fn part_two(&self, _input: &str) -> String {
@@ -12,12 +28,44 @@ impl Solution for Day09 {
     }
 }
 
+impl Day09 {
+    fn parse(&self, input: &str) -> Distances {
+        input
+            .lines()
+            .flat_map(|line| {
+                let parts = line.split_whitespace().collect_vec();
+
+                let from = parts[0];
+                let to = parts[2];
+                let distance = parts[4].parse::<u64>().unwrap();
+
+                [
+                    ((from.to_string(), to.to_string()), distance),
+                    ((to.to_string(), from.to_string()), distance),
+                ]
+            })
+            .collect()
+    }
+
+    fn cities(&self, distances: &Distances) -> Vec<String> {
+        distances
+            .keys()
+            .flat_map(|(from, to)| [from.to_string(), to.to_string()])
+            .unique()
+            .collect()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const EXAMPLE: &str = r#"London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141"#;
+
     #[test]
     fn part_one_example_test() {
-        assert_eq!("0", Day09.part_one(""));
+        assert_eq!("605", Day09.part_one(EXAMPLE));
     }
 }
