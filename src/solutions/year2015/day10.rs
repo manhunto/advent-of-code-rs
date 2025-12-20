80951160217fc1bc351a70b6070c1c7f3eabc6d8
Jakub Sładek <jakub.sladek@zoho.com>
2025-12-20 12:29:21 +0100

    feat(10/2015): solve first part

--- src/solutions/year2015/day10.rs
+++ src/solutions/year2015/day10.rs
@@ -3,12 +3,55 @@ use crate::solutions::Solution;
 pub struct Day10;
 
 impl Solution for Day10 {
-    fn part_one(&self, _input: &str) -> String {
-        String::from("0")
+    fn part_one(&self, input: &str) -> String {
+        self.look_and_say_for_string(input, 40).len().to_string()
     }
 
     fn part_two(&self, _input: &str) -> String {
         String::from("0")
+    }
+}
+
+impl Day10 {
+    fn look_and_say_for_string(&self, input: &str, times: usize) -> String {
+        let mut numbers: Vec<u8> = input
+            .trim()
+            .chars()
+            .map(|c| c.to_string().parse::<u8>().unwrap())
+            .collect();
+
+        for _ in 0..times {
+            numbers = self.look_and_say(&numbers);
+        }
+
+        numbers.iter().map(|u| u.to_string()).collect::<String>()
+    }
+
+    fn look_and_say(&self, numbers: &[u8]) -> Vec<u8> {
+        let mut current = numbers[0];
+        let mut count = 1u8;
+        let mut index = 1;
+        let mut vec: Vec<u8> = Vec::new();
+
+        while let Some(value) = numbers.get(index) {
+            if *value == current {
+                count += 1;
+                index += 1;
+                continue;
+            } else {
+                vec.push(count);
+                vec.push(current);
+
+                count = 1;
+                index += 1;
+                current = *value;
+            }
+        }
+
+        vec.push(count);
+        vec.push(current);
+
+        vec
     }
 }
 
@@ -17,7 +60,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part_one_example_test() {
-        assert_eq!("0", Day10.part_one("0"));
+    fn look_and_say() {
+        assert_eq!("11", Day10.look_and_say_for_string("1", 1));
+        assert_eq!("21", Day10.look_and_say_for_string("11", 1));
+        assert_eq!("1211", Day10.look_and_say_for_string("21", 1));
+        assert_eq!("111221", Day10.look_and_say_for_string("1211", 1));
+        assert_eq!("312211", Day10.look_and_say_for_string("111221", 1));
     }
 }
