30236d0be55e101422c42805a3808a0ced8412a5
Jakub Sładek <jakub.sladek@zoho.com>
2025-12-21 16:13:28 +0100

    feat(12/2015): solve first part

--- src/solutions/year2015/day12.rs
+++ src/solutions/year2015/day12.rs
@@ -3,8 +3,16 @@ use crate::solutions::Solution;
 pub struct Day12;
 
 impl Solution for Day12 {
-    fn part_one(&self, _input: &str) -> String {
-        String::from("0")
+    fn part_one(&self, input: &str) -> String {
+        input
+            .trim()
+            .split(|char| {
+                ['[', ']', '{', '}', ',', '"', ':'].contains(&char) || char.is_alphabetic()
+            })
+            .filter(|s| !s.is_empty())
+            .map(|d| d.parse::<i64>().unwrap())
+            .sum::<i64>()
+            .to_string()
     }
 
     fn part_two(&self, _input: &str) -> String {
@@ -18,6 +26,12 @@ mod tests {
 
     #[test]
     fn part_one_example_test() {
-        assert_eq!("0", Day12.part_one("0"));
+        assert_eq!("6", Day12.part_one("[1,2,3]"));
+        assert_eq!("6", Day12.part_one(r#"{"a":2,"b":4}"#));
+        assert_eq!("3", Day12.part_one(r#"[[[3]]]"#));
+        assert_eq!("3", Day12.part_one(r#"{"a":{"b":4},"c":-1}"#));
+        assert_eq!("0", Day12.part_one(r#"{"a":[-1,1]}"#));
+        assert_eq!("0", Day12.part_one(r#"[-1,{"a":1}]"#));
+        assert_eq!("0", Day12.part_one(r#"{}"#));
     }
 }
