dfea60422261404ada8da6b53852f8368a49fd89
Jakub Sładek <jakub.sladek@zoho.com>
2025-12-30 00:48:42 +0100

    refactor(19/2015): improve readability

--- src/solutions/year2015/day19.rs
+++ src/solutions/year2015/day19.rs
@@ -5,11 +5,23 @@ pub struct Day19;
 
 impl Solution for Day19 {
     fn part_one(&self, input: &str) -> String {
+        let (replacements, word) = self.parse(input);
+        let mut new_words = HashSet::new();
+        new_words.extend(self.search_for_replacement(&replacements, word.clone(), 1));
+        new_words.extend(self.search_for_replacement(&replacements, word, 2));
+
+        new_words.len().to_string()
+    }
+
+    fn part_two(&self, _input: &str) -> String {
+        String::from("0")
+    }
+}
+
+impl Day19 {
+    fn parse(&self, input: &str) -> (HashMap<String, Vec<String>>, String) {
         let (replacements_str, word) = input.split_once("\n\n").unwrap();
         let mut replacements: HashMap<String, Vec<String>> = HashMap::new();
-        let word = word.to_string();
-
-        let mut count = 0usize;
 
         for replacement in replacements_str.lines() {
             let (from, to) = replacement.split_once(" => ").unwrap();
@@ -18,41 +30,35 @@ impl Solution for Day19 {
                 .entry(from.to_string())
                 .or_default()
                 .push(to.to_string());
-
-            count += 1;
         }
 
-        let mut new_words = HashSet::with_capacity(count);
+        (replacements, word.to_string())
+    }
 
-        for (i, w) in word.char_indices() {
-            if let Some(char_replacements) = replacements.get(&w.to_string()) {
-                for char_replacement in char_replacements {
-                    let mut new_word = word.clone();
+    fn search_for_replacement(
+        &self,
+        replacements: &HashMap<String, Vec<String>>,
+        word: String,
+        length: usize,
+    ) -> HashSet<String> {
+        let mut new_words = HashSet::new();
 
-                    new_word.replace_range(i..i + 1, char_replacement);
-                    new_words.insert(new_word);
-                }
-            }
+        for i in 0..word.len() {
+            if i <= word.len() - length {
+                let pattern = word[i..i + length].to_string();
 
-            if i < word.len() - 1 {
-                let two = word[i..i + 2].to_string();
-
-                if let Some(char_replacements) = replacements.get(&two) {
+                if let Some(char_replacements) = replacements.get(&pattern) {
                     for char_replacement in char_replacements {
                         let mut new_word = word.clone();
 
-                        new_word.replace_range(i..i + two.len(), char_replacement);
+                        new_word.replace_range(i..i + pattern.len(), char_replacement);
                         new_words.insert(new_word);
                     }
                 }
             }
         }
 
-        new_words.len().to_string()
-    }
-
-    fn part_two(&self, _input: &str) -> String {
-        String::from("0")
+        new_words
     }
 }
 
