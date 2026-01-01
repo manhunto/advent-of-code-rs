7fbfd1b76159f76bb5d4c0bc090aade5932f3076
Jakub Sładek <jakub.sladek@zoho.com>
2026-01-01 19:37:41 +0100

    refactor(19/2015): prepare for part two

--- src/solutions/year2015/day19.rs
+++ src/solutions/year2015/day19.rs
@@ -1,5 +1,6 @@
 use crate::solutions::Solution;
 use std::collections::{HashMap, HashSet};
+use std::ops::Range;
 
 pub struct Day19;
 
@@ -7,8 +8,9 @@ impl Solution for Day19 {
     fn part_one(&self, input: &str) -> String {
         let (replacements, word) = self.parse(input);
         let mut new_words = HashSet::new();
-        self.search_for_replacement(&replacements, word, 1, &mut new_words);
-        self.search_for_replacement(&replacements, word, 2, &mut new_words);
+
+        self.search_for_replacement(&replacements, &word, 1, &mut new_words);
+        self.search_for_replacement(&replacements, &word, 2, &mut new_words);
 
         new_words.len().to_string()
     }
@@ -19,7 +21,7 @@ impl Solution for Day19 {
 }
 
 impl Day19 {
-    fn parse<'a>(&self, input: &'a str) -> (HashMap<&'a str, Vec<&'a str>>, &'a str) {
+    fn parse<'a>(&self, input: &'a str) -> (HashMap<&'a str, Vec<&'a str>>, Word) {
         let (replacements_str, word) = input.split_once("\n\n").unwrap();
         let mut replacements: HashMap<&str, Vec<&str>> = HashMap::new();
 
@@ -28,25 +30,55 @@ impl Day19 {
             replacements.entry(from).or_default().push(to);
         }
 
-        (replacements, word.trim())
+        (replacements, Word::new(word))
     }
 
     fn search_for_replacement(
         &self,
         replacements: &HashMap<&str, Vec<&str>>,
-        word: &str,
+        word: &Word,
         length: usize,
-        new_words: &mut HashSet<String>,
+        new_words: &mut HashSet<Word>,
     ) {
-        for i in 0..=word.len().saturating_sub(length) {
-            let pattern = &word[i..i + length];
+        let str = word.word.as_str();
+
+        for i in 0..=str.len().saturating_sub(length) {
+            let pattern = &str[i..i + length];
 
             if let Some(char_replacements) = replacements.get(pattern) {
                 for &replacement in char_replacements {
-                    let new_word = format!("{}{}{}", &word[..i], replacement, &word[i + length..]);
-                    new_words.insert(new_word);
+                    new_words.insert(word.replace(i..i + length, replacement));
                 }
             }
+        }
+    }
+}
+
+#[derive(Eq, Hash, PartialEq)]
+struct Word {
+    word: String,
+    replacements: usize,
+}
+
+impl Word {
+    fn new(word: &str) -> Self {
+        Self {
+            word: word.to_string(),
+            replacements: 0,
+        }
+    }
+
+    fn replace(&self, i: Range<usize>, replacement: &str) -> Self {
+        let word = format!(
+            "{}{}{}",
+            &self.word[..i.start],
+            replacement,
+            &self.word[i.end..]
+        );
+
+        Self {
+            word,
+            replacements: self.replacements + 1,
         }
     }
 }
@@ -84,5 +116,67 @@ HOHOHO"#;
     #[test]
     fn part_one_two_letters() {
         assert_eq!("3", Day19.part_one(EXAMPLE_TWO_LETTERS));
+    }
+
+    const EXAMPLE_TWO_LETTERS_TO_ONE: &str = r#"HO => H
+
+HOHOHO"#;
+
+    #[test]
+    fn part_one_two_letters_to_one() {
+        assert_eq!("3", Day19.part_one(EXAMPLE_TWO_LETTERS_TO_ONE));
+    }
+
+    #[test]
+    fn one_to_many() {
+        let input = r#"A => ABC
+
+AAA"#;
+        assert_eq!("3", Day19.part_one(input));
+    }
+
+    #[test]
+    fn same_position_different_lengths() {
+        let input = r#"A => X
+AB => Y
+
+ABC"#;
+        assert_eq!("2", Day19.part_one(input));
+    }
+
+    #[test]
+    fn replacement_at_end() {
+        let input = r#"C => XYZ
+
+ABC"#;
+        assert_eq!("1", Day19.part_one(input));
+    }
+
+    #[test]
+    fn replacement_at_start() {
+        let input = r#"A => XYZ
+
+ABC"#;
+        assert_eq!("1", Day19.part_one(input));
+    }
+
+    const REPLACEMENTS_PART_TWO: &str = r#"e => H
+e => O
+H => HO
+H => OH
+O => HH"#;
+
+    #[test]
+    fn part_two_example_one() {
+        let input = format!("{}\n\n{}", REPLACEMENTS_PART_TWO, "HOH");
+
+        assert_eq!("3", Day19.part_two(&input));
+    }
+
+    #[test]
+    fn part_two_example_two() {
+        let input = format!("{}\n\n{}", REPLACEMENTS_PART_TWO, "HOHOHO");
+
+        assert_eq!("6", Day19.part_two(&input));
     }
 }
