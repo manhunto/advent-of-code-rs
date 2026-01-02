17616785a95ed538664db8a5dd00d09f654c16f1
Kuba Sładek <jakub.sladek@zoho.com>
2026-01-02 14:22:57 +0100

    refactor(21/2015): simplify, improve readability, make more rusty

--- src/solutions/year2015/day21.rs
+++ src/solutions/year2015/day21.rs
@@ -1,9 +1,8 @@
 use crate::solutions::Solution;
-use itertools::Itertools;
-use std::num::ParseIntError;
+use itertools::{iproduct, Itertools};
 use std::str::FromStr;
 
-const ITEMS_LIST: &str = r#"Weapons:    Cost  Damage  Armor
+const SHOP_DATA: &str = r#"Weapons:    Cost  Damage  Armor
 Dagger        8     4       0
 Shortsword   10     5       0
 Warhammer    25     6       0
@@ -18,164 +17,127 @@ Bandedmail   75     0       4
 Platemail   102     0       5
 
 Rings:      Cost  Damage  Armor
-Damage+1     25     1       0
-Damage+2     50     2       0
-Damage+3    100     3       0
-Defense+1    20     0       1
-Defense+2    40     0       2
-Defense+3    80     0       3"#;
+Damage +1     25     1       0
+Damage +2     50     2       0
+Damage +3    100     3       0
+Defense +1    20     0       1
+Defense +2    40     0       2
+Defense +3    80     0       3"#;
+
+const MIN_DAMAGE: u64 = 1;
+const DEFAULT_PLAYER_HP: u64 = 100;
 
 pub struct Day21;
 
 impl Solution for Day21 {
     fn part_one(&self, input: &str) -> String {
-        self.solve(input, Strategy::CheapestWinning)
+        let boss: Mob = input.parse().unwrap();
+
+        self.all_player_loadouts()
+            .filter(|set| {
+                let player: Mob = set.into();
+
+                player.wins(&boss)
+            })
+            .map(|set| set.cost())
+            .min()
+            .unwrap_or(0)
+            .to_string()
     }
 
     fn part_two(&self, input: &str) -> String {
-        self.solve(input, Strategy::MostExpensiveLoosing)
+        let boss: Mob = input.parse().unwrap();
+
+        self.all_player_loadouts()
+            .filter(|set| {
+                let player: Mob = set.into();
+
+                !player.wins(&boss)
+            })
+            .map(|set| set.cost())
+            .max()
+            .unwrap_or(0)
+            .to_string()
     }
 }
 
 impl Day21 {
-    fn parse_item_list(&self) -> (Vec<Item>, Vec<Option<Item>>, Vec<Item>) {
-        let (weapons_str, armor_str, rings_str) =
-            ITEMS_LIST.split_terminator("\n\n").collect_tuple().unwrap();
+    fn all_player_loadouts(&self) -> impl Iterator<Item = Set> {
+        let (weapons, armors, rings) = self.parse_shop();
 
-        let mut armors: Vec<Option<Item>> = self.parse_items(armor_str).map(Some).collect();
-        armors.push(None);
+        iproduct!(weapons, armors, rings.into_iter().tuple_combinations())
+            .map(|(w, a, (r1, r2))| Set::new(w, a, vec![r1, r2]))
+    }
+    fn parse_shop(&self) -> (Vec<Item>, Vec<Item>, Vec<Item>) {
+        let (weapons_str, armor_str, rings_str) = SHOP_DATA.split("\n\n").collect_tuple().unwrap();
 
-        (
-            self.parse_items(weapons_str).collect(),
-            armors,
-            self.parse_items(rings_str).collect(),
-        )
+        let weapons = self.parse_items(weapons_str);
+
+        let mut armors = self.parse_items(armor_str);
+        armors.push(Item::default());
+
+        let mut rings = self.parse_items(rings_str);
+        rings.push(Item::default());
+        rings.push(Item::default());
+
+        (weapons, armors, rings)
     }
 
-    fn parse_items<'a>(&self, items: &'a str) -> impl Iterator<Item = Item> + 'a {
-        items.lines().skip(1).map(|line| line.parse().unwrap())
-    }
-
-    fn parse_input(&self, input: &str) -> Character {
-        let lines = input.lines().collect_vec();
-
-        let hit_points = self.parse_boss_stat(lines[0]);
-        let damage = self.parse_boss_stat(lines[1]);
-        let armor = self.parse_boss_stat(lines[2]);
-
-        Character::new(hit_points, damage, armor)
-    }
-
-    fn parse_boss_stat(&self, line: &str) -> u64 {
-        let (_, n) = line.split_once(": ").unwrap();
-
-        n.parse().unwrap()
-    }
-
-    fn rings_combinations(&self, rings: Vec<Item>) -> Vec<Vec<Item>> {
-        let mut combinations = Vec::new();
-        combinations.push(vec![]);
-
-        for ring_count in 1..=2 {
-            combinations.extend(rings.clone().into_iter().combinations(ring_count));
-        }
-
-        combinations
-    }
-
-    fn solve(&self, input: &str, strategy: Strategy) -> String {
-        let (weapons, armors, rings) = self.parse_item_list();
-        let ring_combinations = self.rings_combinations(rings);
-
-        let boss = self.parse_input(input);
-        let mut best: u64 = strategy.init();
-
-        for weapon in weapons {
-            for armor in armors.iter() {
-                for rings_set in ring_combinations.iter() {
-                    let set = Set::new(weapon, *armor, rings_set.clone());
-                    let set_cost = set.cost();
-                    let player: Character = set.into();
-
-                    let result = player.wins(&boss);
-                    if strategy.accepts(result) {
-                        best = strategy.pick(best, set_cost);
-                    }
-                }
-            }
-        }
-
-        best.to_string()
+    fn parse_items(&self, items: &str) -> Vec<Item> {
+        items
+            .lines()
+            .skip(1)
+            .map(|line| line.parse().unwrap())
+            .collect()
     }
 }
 
-enum Strategy {
-    CheapestWinning,
-    MostExpensiveLoosing,
-}
-
-impl Strategy {
-    fn init(&self) -> u64 {
-        match self {
-            Strategy::CheapestWinning => u64::MAX,
-            Strategy::MostExpensiveLoosing => u64::MIN,
-        }
-    }
-
-    fn accepts(&self, player_wins: bool) -> bool {
-        match self {
-            Strategy::CheapestWinning if player_wins => true,
-            Strategy::MostExpensiveLoosing if !player_wins => true,
-            _ => false,
-        }
-    }
-
-    fn pick(&self, a: u64, b: u64) -> u64 {
-        match self {
-            Strategy::CheapestWinning => a.min(b),
-            Strategy::MostExpensiveLoosing => a.max(b),
-        }
-    }
-}
-
-struct Character {
+struct Mob {
     hit_points: u64,
     damage: u64,
     armor: u64,
 }
 
-impl Character {
-    fn new(hit_points: u64, damage: u64, armor: u64) -> Self {
-        Self {
-            hit_points,
-            damage,
-            armor,
-        }
-    }
-
+impl Mob {
     /// It indicates that self has first turn
-    fn wins(&self, other: &Character) -> bool {
-        let my_damage = self.damage.saturating_sub(other.armor).max(1);
-        let hits_to_kill_other = other.hit_points.div_ceil(my_damage);
+    fn wins(&self, other: &Mob) -> bool {
+        let my_damage = self.damage.saturating_sub(other.armor).max(MIN_DAMAGE);
+        let other_damage = other.damage.saturating_sub(self.armor).max(MIN_DAMAGE);
 
-        let other_damage = other.damage.saturating_sub(self.armor).max(1);
+        let hits_to_kill_other = other.hit_points.div_ceil(my_damage);
         let hits_to_kill_self = self.hit_points.div_ceil(other_damage);
 
         hits_to_kill_other <= hits_to_kill_self
     }
 }
 
-impl From<Set> for Character {
-    fn from(value: Set) -> Self {
+impl From<&Set> for Mob {
+    fn from(value: &Set) -> Self {
         Self {
-            hit_points: 100,
+            hit_points: DEFAULT_PLAYER_HP,
             damage: value.damage(),
             armor: value.armor(),
         }
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+impl FromStr for Mob {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let nums: Vec<u64> = s
+            .lines()
+            .map(|l| l.split(": ").last().unwrap().parse().unwrap())
+            .collect();
+
+        Ok(Self {
+            hit_points: nums[0],
+            damage: nums[1],
+            armor: nums[2],
+        })
+    }
+}
+
+#[derive(Debug, Copy, Clone, Default)]
 struct Item {
     cost: u64,
     damage: u64,
@@ -183,15 +145,20 @@ struct Item {
 }
 
 impl FromStr for Item {
-    type Err = ParseIntError;
+    type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (_, cost, damage, armor) = s.split_whitespace().collect_tuple().unwrap();
+        let parts: Vec<&str> = s.split_whitespace().collect();
 
-        Ok(Self {
-            cost: cost.parse()?,
-            damage: damage.parse()?,
-            armor: armor.parse()?,
+        let len = parts.len();
+        if len < 3 {
+            return Err(());
+        }
+
+        Ok(Item {
+            cost: parts[len - 3].parse().unwrap(),
+            damage: parts[len - 2].parse().unwrap(),
+            armor: parts[len - 1].parse().unwrap(),
         })
     }
 }
@@ -199,12 +166,12 @@ impl FromStr for Item {
 #[derive(Clone)]
 struct Set {
     weapon: Item,
-    armor: Option<Item>,
+    armor: Item,
     rings: Vec<Item>,
 }
 
 impl Set {
-    fn new(weapon: Item, armor: Option<Item>, rings: Vec<Item>) -> Self {
+    fn new(weapon: Item, armor: Item, rings: Vec<Item>) -> Self {
         assert!(rings.len() <= 2);
 
         Self {
@@ -215,7 +182,7 @@ impl Set {
     }
 
     fn cost(&self) -> u64 {
-        self.weapon.cost + self.sum_armor(|item| item.cost) + self.sum_rings(|item| item.cost)
+        self.weapon.cost + self.armor.cost + self.sum_rings(|item| item.cost)
     }
 
     fn damage(&self) -> u64 {
@@ -223,14 +190,7 @@ impl Set {
     }
 
     fn armor(&self) -> u64 {
-        self.sum_armor(|item| item.armor) + self.sum_rings(|item| item.armor)
-    }
-
-    fn sum_armor<F>(&self, func: F) -> u64
-    where
-        F: Fn(&Item) -> u64,
-    {
-        self.armor.as_ref().map_or(0, func)
+        self.armor.armor + self.sum_rings(|item| item.armor)
     }
 
     fn sum_rings<F>(&self, func: F) -> u64
@@ -245,26 +205,36 @@ impl Set {
 mod tests {
     use super::*;
 
+    impl Mob {
+        fn new(hit_points: u64, damage: u64, armor: u64) -> Self {
+            Self {
+                hit_points,
+                damage,
+                armor,
+            }
+        }
+    }
+
     #[test]
     fn wins_equal_rounds() {
-        let player = Character::new(8, 5, 5);
-        let boss = Character::new(12, 7, 2);
+        let player = Mob::new(8, 5, 5);
+        let boss = Mob::new(12, 7, 2);
 
         assert!(player.wins(&boss));
     }
 
     #[test]
     fn wins_boss_more_hp() {
-        let player = Character::new(8, 5, 5);
-        let boss = Character::new(13, 7, 2);
+        let player = Mob::new(8, 5, 5);
+        let boss = Mob::new(13, 7, 2);
 
         assert!(!player.wins(&boss));
     }
 
     #[test]
     fn wins_boss_more_armor_than_damage_decreases_1hp() {
-        let player = Character::new(100, 20, 2);
-        let boss = Character::new(2, 4, 100);
+        let player = Mob::new(100, 20, 2);
+        let boss = Mob::new(2, 4, 100);
 
         assert!(player.wins(&boss));
     }
@@ -280,8 +250,8 @@ mod tests {
         // Turn 3: Player hits (Boss -1/Dead). Player wins.
 
         // BUG: Current logic compares 2.8 <= 2.4 (False), so it thinks Player loses.
-        let player = Character::new(12, 5, 0);
-        let boss = Character::new(14, 5, 0);
+        let player = Mob::new(12, 5, 0);
+        let boss = Mob::new(14, 5, 0);
 
         assert!(
             player.wins(&boss),
@@ -307,13 +277,13 @@ mod tests {
         let ring1: Item = "Damage+1    25     1       0".parse().unwrap();
         let ring2: Item = "Defense+2   40     0       2".parse().unwrap();
 
-        let set = Set::new(weapon, Some(armor), vec![ring1, ring2]);
+        let set = Set::new(weapon, armor, vec![ring1, ring2]);
 
         assert_eq!(128, set.cost());
         assert_eq!(6, set.damage());
         assert_eq!(5, set.armor());
 
-        let player: Character = set.into();
+        let player: Mob = (&set).into();
 
         assert_eq!(100, player.hit_points);
         assert_eq!(6, player.damage);
