639d73c121ccfbaed2c572e6a48fe235a4f9e0df
Jakub Sładek <jakub.sladek@zoho.com>
2026-01-06 18:15:23 +0100

    refactor(25/2015): improve readability

--- src/solutions/year2015/day25.rs
+++ src/solutions/year2015/day25.rs
@@ -5,27 +5,28 @@ pub struct Day25;
 
 impl Solution for Day25 {
     fn part_one(&self, input: &str) -> String {
-        let parts = input.split_whitespace().collect_vec();
-        let row: usize = parts[15].trim_end_matches(',').parse().unwrap();
-        let col: usize = parts[17].trim_end_matches('.').parse().unwrap();
+        let (row, col) = self.parse(input);
 
         InfinityPaper::new()
             .into_iter()
-            .find_map(
-                |(r, c, v)| {
-                    if r == row && c == col {
-                        Some(v)
-                    } else {
-                        None
-                    }
-                },
-            )
-            .unwrap_or(0)
+            .find(|(r, c, _)| *r == row && *c == col)
+            .unwrap()
+            .2
             .to_string()
     }
 
     fn part_two(&self, _input: &str) -> String {
         String::from("0")
+    }
+}
+
+impl Day25 {
+    fn parse(&self, input: &str) -> (usize, usize) {
+        let parts = input.split_whitespace().collect_vec();
+        let row: usize = parts[15].trim_end_matches(',').parse().unwrap();
+        let col: usize = parts[17].trim_end_matches('.').parse().unwrap();
+
+        (row, col)
     }
 }
 
@@ -83,5 +84,12 @@ mod tests {
         assert_eq!(Some((3, 2, 8057251)), infinity.next());
         assert_eq!(Some((2, 3, 16929656)), infinity.next());
         assert_eq!(Some((1, 4, 30943339)), infinity.next());
+    }
+
+    const INPUT: &str = "To continue, please consult the code grid in the manual.  Enter the code at row 6, column 6.";
+
+    #[test]
+    fn part_one() {
+        assert_eq!(Day25.part_one(INPUT), "27995004");
     }
 }
