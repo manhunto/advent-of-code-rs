99a8f250ebe3ec07b7750e791c27ee6548c045fa
Jakub Sładek <jakub.sladek@zoho.com>
2026-01-06 15:23:34 +0100

    refactor(24/2015): dont iterate over all results and return early on first found chunk; still wrong answer

--- src/solutions/year2015/day24.rs
+++ src/solutions/year2015/day24.rs
@@ -1,6 +1,5 @@
 use crate::solutions::Solution;
 use itertools::Itertools;
-use std::collections::HashSet;
 
 pub struct Day24;
 
@@ -22,67 +21,45 @@ impl Day24 {
     }
 
     fn solve(&self, weights: &[u32]) -> u32 {
-        let chunks = self.possible_chunks(weights);
-
-        let mut candidates: Vec<Vec<u32>> = Vec::new();
-        let mut number_of_packages: Option<usize> = None;
-
-        for i in 0..chunks.len() {
-            if self.can_be_in_first_group(&chunks, i, weights.len()) {
-                let candidate = &chunks[i];
-
-                if number_of_packages.is_none_or(|v| v == candidate.len()) {
-                    number_of_packages = Some(i);
-                    candidates.push(candidate.to_vec());
-                }
-
-                if number_of_packages.is_some_and(|v| v < candidate.len()) {
-                    break;
-                }
-            }
-        }
-
-        candidates.iter().map(|c| c.iter().product()).min().unwrap()
-    }
-
-    fn can_be_in_first_group(&self, chunks: &[Vec<u32>], index: usize, weights_len: usize) -> bool {
-        let a = &chunks[index];
-
-        for i in (index + 1)..chunks.len() {
-            let b = &chunks[i];
-
-            for c in chunks.iter().skip(i + 1) {
-                let mut set: HashSet<&u32> = HashSet::with_capacity(a.len() + b.len() + c.len());
-                set.extend(a);
-                set.extend(b);
-                set.extend(c);
-
-                if set.len() == weights_len {
-                    return true;
-                }
-            }
-        }
-
-        false
-    }
-
-    fn possible_chunks(&self, weights: &[u32]) -> Vec<Vec<u32>> {
-        let mut results = Vec::new();
         let sum: u32 = weights.iter().sum::<u32>() / 3;
+        let max_chunk_length = weights.len() - 1;
 
-        for chunk_length in 1..weights.len() - 1 {
-            let combinations = weights.iter().combinations(chunk_length);
+        for chunk_length in 1..max_chunk_length {
+            let candidates: Vec<_> = weights
+                .iter()
+                .combinations(chunk_length)
+                .filter(|a| a.iter().map(|x| **x).sum::<u32>() == sum)
+                .filter(|candidate| {
+                    let mut left_to_share = weights.to_vec();
+                    left_to_share.retain(|v| !candidate.contains(&v));
 
-            for combination in combinations {
-                let chunk_sum: u32 = combination.iter().copied().sum();
+                    let is_valid = (candidate.len()..max_chunk_length).any(|b_chunk_length| {
+                        left_to_share
+                            .iter()
+                            .combinations(b_chunk_length)
+                            .filter(|b| b.iter().map(|x| **x).sum::<u32>() == sum)
+                            .any(|b| {
+                                let mut last_chunk = left_to_share.to_vec();
+                                last_chunk.retain(|v| !b.contains(&v));
 
-                if chunk_sum == sum {
-                    results.push(combination.into_iter().copied().collect());
-                }
+                                last_chunk.iter().sum::<u32>() == sum
+                            })
+                    });
+
+                    is_valid
+                })
+                .collect();
+
+            if !candidates.is_empty() {
+                return candidates
+                    .into_iter()
+                    .map(|candidate| candidate.into_iter().product::<u32>())
+                    .min()
+                    .unwrap_or(0);
             }
         }
 
-        results
+        unreachable!()
     }
 }
 
@@ -91,11 +68,6 @@ mod tests {
     use super::*;
 
     const EXAMPLE: [u32; 10] = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11];
-
-    #[test]
-    fn possible_chunks() {
-        assert_eq!(25, Day24.possible_chunks(&EXAMPLE).len());
-    }
 
     #[test]
     fn part_one_example() {
