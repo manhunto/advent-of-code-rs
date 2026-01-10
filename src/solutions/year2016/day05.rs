a4330b8a85d8eca208e6610a5c90ad3d249ef35e
Jakub Sładek <jakub.sladek@zoho.com>
2026-01-10 21:05:19 +0100

    feat(05/2016): solve first part, but it is very slow

--- src/solutions/year2016/day05.rs
+++ src/solutions/year2016/day05.rs
@@ -1,10 +1,23 @@
 use crate::solutions::Solution;
+use md5::compute;
 
 pub struct Day05;
 
 impl Solution for Day05 {
-    fn part_one(&self, _input: &str) -> String {
-        String::from("0")
+    fn part_one(&self, input: &str) -> String {
+        let door_id = input.trim();
+
+        (0u64..)
+            .map(|n| {
+                let hash = format!("{}{}", door_id, n);
+                let digest = compute(hash);
+
+                format!("{:x}", digest)
+            })
+            .filter(|x| x.starts_with("00000"))
+            .take(8)
+            .map(|str| str.chars().nth(5).unwrap())
+            .collect::<String>()
     }
 
     fn part_two(&self, _input: &str) -> String {
@@ -14,12 +27,11 @@ impl Solution for Day05 {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    const EXAMPLE: &str = r#""#;
-
-    #[test]
-    fn part_one_example() {
-        assert_eq!("0", Day05.part_one(EXAMPLE));
-    }
+    // todo: it is very slow
+    // use super::*;
+    //
+    // #[test]
+    // fn part_one_example() {
+    //     assert_eq!("18f47a30", Day05.part_one("abc"));
+    // }
 }
