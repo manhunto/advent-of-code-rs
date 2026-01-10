d4ab36ee5842d6e00b4f40a93a540a10d10d2e58
Jakub Sładek <jakub.sladek@zoho.com>
2026-01-10 13:30:41 +0100

    feat(03/2016): solve first part

--- src/solutions/year2016/day03.rs
+++ src/solutions/year2016/day03.rs
@@ -1,10 +1,14 @@
 use crate::solutions::Solution;
+use itertools::Itertools;
 
 pub struct Day03;
 
 impl Solution for Day03 {
-    fn part_one(&self, _input: &str) -> String {
-        String::from("0")
+    fn part_one(&self, input: &str) -> String {
+        self.parse(input)
+            .filter(|lengths| self.is_valid_triangle(lengths))
+            .count()
+            .to_string()
     }
 
     fn part_two(&self, _input: &str) -> String {
@@ -12,14 +16,35 @@ impl Solution for Day03 {
     }
 }
 
+impl Day03 {
+    fn parse<'a>(&self, input: &'a str) -> impl Iterator<Item = [u16; 3]> + 'a {
+        input.lines().map(|line| {
+            let (a, b, c) = line
+                .split_whitespace()
+                .map(|s| s.parse::<u16>().unwrap())
+                .collect_tuple()
+                .unwrap();
+
+            [a, b, c]
+        })
+    }
+
+    fn is_valid_triangle(&self, lengths: &[u16; 3]) -> bool {
+        let mut l = *lengths;
+        l.sort_unstable();
+
+        l[2] < l[0] + l[1]
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = r#""#;
-
     #[test]
-    fn part_one_example() {
-        assert_eq!("0", Day03.part_one(EXAMPLE));
+    fn is_valid_triangle3() {
+        assert!(!Day03.is_valid_triangle(&[5, 10, 25]));
+        assert!(!Day03.is_valid_triangle(&[5, 10, 15]));
+        assert!(Day03.is_valid_triangle(&[5, 10, 14]));
     }
 }
