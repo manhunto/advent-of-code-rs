a3f169c5aaa6cec21a7bc6fba978d11e6883e38f
Jakub Sładek <jakub.sladek@zoho.com>
2026-01-11 23:55:20 +0100

    feat(09/2016): solve first part

--- src/solutions/year2016/day09.rs
+++ src/solutions/year2016/day09.rs
@@ -3,8 +3,38 @@ use crate::solutions::Solution;
 pub struct Day09;
 
 impl Solution for Day09 {
-    fn part_one(&self, _input: &str) -> String {
-        String::from("0")
+    fn part_one(&self, input: &str) -> String {
+        let mut i = 0;
+        let mut length = 0;
+        let chars = input.trim().chars().collect::<Vec<char>>();
+
+        while i < chars.len() {
+            if chars[i].is_ascii_uppercase() {
+                length += 1;
+            }
+
+            if chars[i] == '(' {
+                let mut capture = Vec::new();
+                let mut j = i + 1;
+
+                while chars[j] != ')' {
+                    capture.push(chars[j]);
+                    j += 1;
+                }
+
+                let marker = capture.iter().collect::<String>();
+                let (c, t) = marker.split_once('x').unwrap();
+                let count = c.parse::<usize>().unwrap();
+                let times = t.parse::<usize>().unwrap();
+
+                length += count * times;
+                i = j + count;
+            }
+
+            i += 1;
+        }
+
+        length.to_string()
     }
 
     fn part_two(&self, _input: &str) -> String {
@@ -16,10 +46,13 @@ impl Solution for Day09 {
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = r#""#;
-
     #[test]
     fn part_one_example() {
-        assert_eq!("0", Day09.part_one(EXAMPLE));
+        assert_eq!("6", Day09.part_one("ADVENT"));
+        assert_eq!("7", Day09.part_one("A(1x5)BC"));
+        assert_eq!("9", Day09.part_one("(3x3)XYZ"));
+        assert_eq!("11", Day09.part_one("A(2x2)BCD(2x2)EFG"));
+        assert_eq!("6", Day09.part_one("(6x1)(1x3)A"));
+        assert_eq!("18", Day09.part_one("X(8x2)(3x3)ABCY"));
     }
 }
