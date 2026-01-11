3bec3ac24dbdc895824c1ca1832fa7b901cd1778
Jakub Sładek <jakub.sladek@zoho.com>
2026-01-11 12:35:56 +0100

    feat(06/2016): solve second part

--- src/solutions/year2016/day06.rs
+++ src/solutions/year2016/day06.rs
@@ -6,22 +6,35 @@ pub struct Day06;
 
 impl Solution for Day06 {
     fn part_one(&self, input: &str) -> String {
-        let mut vec: HashMap<usize, HashMap<char, usize>> = HashMap::new();
-
-        for line in input.lines() {
-            for (column, char) in line.char_indices() {
-                *vec.entry(column).or_default().entry(char).or_insert(0) += 1;
-            }
-        }
-
-        vec.iter()
-            .sorted_by_key(|(k, _)| *k)
-            .map(|(_, map)| map.iter().max_by_key(|(_, v)| *v).unwrap().0)
+        self.group(input)
+            .map(|map| *map.iter().max_by_key(|(_, v)| *v).unwrap().0)
             .collect()
     }
 
-    fn part_two(&self, _input: &str) -> String {
-        String::from("0")
+    fn part_two(&self, input: &str) -> String {
+        self.group(input)
+            .map(|map| *map.iter().min_by_key(|(_, v)| *v).unwrap().0)
+            .collect()
+    }
+}
+
+impl Day06 {
+    fn group(&self, input: &str) -> impl Iterator<Item = HashMap<char, usize>> {
+        let mut columns: HashMap<usize, HashMap<char, usize>> = HashMap::new();
+
+        for line in input.lines() {
+            for (column, char) in line.char_indices() {
+                *columns.entry(column).or_default().entry(char).or_insert(0) += 1;
+            }
+        }
+
+        let sorted: Vec<_> = columns
+            .into_iter()
+            .sorted_by_key(|(k, _)| *k)
+            .map(|(_, map)| map)
+            .collect();
+
+        sorted.into_iter()
     }
 }
 
@@ -49,5 +62,10 @@ enarar"#;
     #[test]
     fn part_one_example() {
         assert_eq!("easter", Day06.part_one(EXAMPLE));
+    }
+
+    #[test]
+    fn part_two_example() {
+        assert_eq!("advent", Day06.part_two(EXAMPLE));
     }
 }
